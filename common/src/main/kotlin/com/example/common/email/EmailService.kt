7d1774772a18d05ff7ab9ebf5cb976d2af59153b
Fernando Guevara <fexsor@gmail.com>
2021-06-18 20:22:17 -0500

    New email option Added send email with attachment

--- common/src/main/kotlin/com/example/common/email/EmailService.kt
+++ common/src/main/kotlin/com/example/common/email/EmailService.kt
@@ -2,9 +2,18 @@ package com.example.common.email
 
 import com.example.api.application.services.IEmailService
 import org.springframework.beans.factory.annotation.Value
+import org.springframework.core.io.ByteArrayResource
 import org.springframework.mail.SimpleMailMessage
 import org.springframework.mail.javamail.JavaMailSender
+import org.springframework.mail.javamail.MimeMessageHelper
 import org.springframework.stereotype.Service
+import java.nio.file.Files
+import java.nio.file.Path
+import java.nio.file.Paths
+import javax.mail.internet.MimeMessage
+
+
+
 
 
 @Service
@@ -23,5 +32,20 @@ class EmailService(
         email.setSubject(subject)
         email.setText(message)
         emailSender.send(email);
+    }
+
+    override fun sendEmail(to: String, subject: String, message: String, fileName: String)
+    {
+        val path: Path = Paths.get(fileName)
+        val content: ByteArray = Files.readAllBytes(path)
+
+        val mimeMessage: MimeMessage = emailSender.createMimeMessage()
+        val email = MimeMessageHelper(mimeMessage, true)
+        email.setFrom(from)
+        email.setTo(to)
+        email.setSubject(subject)
+        email.setText(message)
+        email.addAttachment(fileName, ByteArrayResource(content))
+        emailSender.send(mimeMessage)
     }
 }
--- common/src/main/kotlin/com/example/common/email/IEmailService.kt
+++ common/src/main/kotlin/com/example/common/email/IEmailService.kt
@@ -3,4 +3,5 @@ package com.example.api.application.services
 interface IEmailService {
 
     fun sendEmail(to: String, subject: String, message: String)
+    fun sendEmail(to: String, subject: String, message: String, fileName: String)
 }
